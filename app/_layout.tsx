4afb781e2d786fa4693884007b16bce47d993af2
Amber Jain <jainanuamber@gmail.com>
2025-11-15 15:59:22 +0530

    added fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,27 @@
 import '../global.css';
 
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
 import { Stack } from 'expo-router';
 
 export default function Layout() {
+  const [loaded, error] = useFonts({
+    'ElmsSans-Regular': require('../assets/fonts/ElmsSans-Regular.ttf'),
+    'ElmsSans-Bold': require('../assets/fonts/ElmsSans-Bold.ttf'),
+    'ElmsSans-SemiBold': require('../assets/fonts/ElmsSans-SemiBold.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
