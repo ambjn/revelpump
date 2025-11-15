1f536b838212f572eb669e6d22b623ba433b7cde
Amber Jain <jainanuamber@gmail.com>
2025-11-15 17:02:20 +0530

    updated header

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,7 +7,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          headerShown: false,
           tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
         }}
       />
