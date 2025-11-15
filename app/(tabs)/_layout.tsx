c03d71a63d3ce89d6ebe31013928f8d2df6a2156
Amber Jain <jainanuamber@gmail.com>
2025-11-15 18:59:30 +0530

    ui changes in tabs layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,21 +1,39 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+import AntDesign from '@expo/vector-icons/AntDesign';
+
 import { Tabs } from 'expo-router';
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: '#00FF77' }}>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: '#EF372EFF',
+        tabBarInactiveTintColor: '#9ca3af',
+        headerShown: false,
+        tabBarStyle: {
+          borderTopWidth: 1,
+          borderTopColor: '#e5e7eb',
+          height: 70,
+          paddingBottom: 8,
+          paddingTop: 8,
+        },
+        tabBarLabelStyle: {
+          fontFamily: 'ElmsSans-SemiBold',
+          fontSize: 15,
+        },
+      }}>
       <Tabs.Screen
         name="index"
         options={{
-          headerShown: false,
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+          title: 'Pumps',
+          tabBarIcon: ({ color }) => <AntDesign name="thunderbolt" size={24} color={color} />,
         }}
       />
       <Tabs.Screen
         name="settings"
         options={{
           title: 'Settings',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
+          tabBarIcon: ({ color }) => <FontAwesome size={24} name="cog" color={color} />,
         }}
       />
     </Tabs>
